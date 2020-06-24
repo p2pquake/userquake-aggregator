3442885aaddbc0761dac2a1ca066f6ee49661941
typewriter <takuyaxp@gmail.com>
2020-06-24 21:51:15 +0900

    Define evaluate.Result

--- pkg/evaluate/compatible.go
+++ pkg/evaluate/compatible.go
@@ -3,13 +3,13 @@ package evaluate
 import (
 	"log"
 
-	"github.com/p2pquake/userquake-aggregator/pkg/epsp"
+	"github.com/p2pquake/userquake-aggregator/pkg/aggregate"
 )
 
 type CompatibleEvaluator struct {
 }
 
-func (c CompatibleEvaluator) Evaluate(records []epsp.Record) Result {
+func (c CompatibleEvaluator) Evaluate(result aggregate.Result) Result {
 	log.Fatalln("Not implemented")
 	return Result{}
 }
--- pkg/evaluate/result.go
+++ pkg/evaluate/result.go
@@ -1,4 +1,17 @@
 package evaluate
 
+import "github.com/p2pquake/userquake-aggregator/pkg/epsp"
+
+type Confidence float64
+
 type Result struct {
+	StartedAt      epsp.EPSPTime
+	Confidence     Confidence
+	AreaConfidence map[epsp.AreaCode]Confidence
+}
+
+type AreaResult struct {
+	Confidence Confidence
+	Count      int
+	Display    string
 }
