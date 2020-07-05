8cf5f5d27bb1e6450c33fdd40ec9d5bca1f7e4c5
typewriter <takuyaxp@gmail.com>
2020-07-05 17:31:25 +0900

    Add eval command (Parse and evaluate data from stdio)

---
+++ cmd/eval.go
@@ -0,0 +1,71 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/p2pquake/userquake-aggregator/pkg/aggregate"
+	"github.com/p2pquake/userquake-aggregator/pkg/epsp"
+	"github.com/p2pquake/userquake-aggregator/pkg/evaluate"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(evalCmd)
+}
+
+var evalCmd = &cobra.Command{
+	Use:   "eval",
+	Short: "Parse data from standard input and output the evaluation result",
+	Run:   execute,
+}
+
+func execute(cmd *cobra.Command, args []string) {
+	// read
+	body, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("Read error: %v", err)
+	}
+
+	// parse
+	var records []interface{}
+	err = json.Unmarshal(body, &records)
+	if err != nil {
+		log.Fatalf("JSON unmarshal error: %v from %v", err, string(body))
+	}
+
+	epspRecords := make([]epsp.Record, len(records))
+	for i, record := range records {
+		bytes, err := json.Marshal(record)
+		if err != nil {
+			log.Fatalf("JSON marshal error: %v", err)
+		}
+
+		epspRecord, err := epsp.Parse(string(bytes))
+		if err != nil {
+			log.Fatalf("epsp.Parse error: %v", err)
+		}
+
+		epspRecords[i] = epspRecord
+	}
+
+	// aggregate & evaluate
+	aggregationResults := aggregate.CompatibleAggregator{}.Aggregate(epspRecords)
+
+	evaluationResults := []evaluate.Result{}
+
+	for _, r := range aggregationResults {
+		result := evaluate.CompatibleEvaluator{}.Evaluate(r)
+		evaluationResults = append(evaluationResults, result)
+	}
+
+	json, err := json.Marshal(evaluationResults)
+	if err != nil {
+		log.Fatalf("JSON marshal error: %v", err)
+	}
+
+	fmt.Println(string(json))
+}
