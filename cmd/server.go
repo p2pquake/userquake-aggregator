3a9c52b281e8301af3c851c725292347a5139a26
typewriter <takuyaxp@gmail.com>
2020-07-07 21:23:21 +0900

    WIP: Add server

---
+++ cmd/server.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+}
+
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Start as server",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello")
+	},
+}
